a33835d176ea44cb4e5fdad10accb092d4adc0a2
Xiaoying Tian <xiaoying@stanford.edu>
2016-04-07 13:21:16 -0700

    the errors seems correct.

--- selection/sampling/randomized/cv_errors.py
+++ selection/sampling/randomized/cv_errors.py
@@ -18,9 +18,9 @@ class Error:
         self.L.fit(self.X, self.y_star)
 
     def cv(self, n_folds=10):
-        k_fold = KFold(self.X.shape[0], n_folds = n_folds)
-        error = 0
         n, p = self.X.shape
+        k_fold = KFold(n, n_folds = n_folds)
+        error = 0
         for train, test in k_fold:
             X_train, y_train = self.X[train,:], self.y[train]
             X_test, y_test = self.X[test,:], self.y[test]
@@ -29,9 +29,9 @@ class Error:
             error += avg_pe(y_test, X_test.dot(ls.coef_)) / n_folds
         return error
 
-    def rand(self, nsim=100):
-        error = 0
+    def rand(self, nsim=50):
         n, p = self.X.shape
+        error = 0
         for _ in range(nsim): 
             rvs = np.random.standard_normal(n)*self.sigma*self.scale
             ls = Lasso(alpha=self.lam/n, fit_intercept=False)
@@ -40,7 +40,7 @@ class Error:
                     (self.sigma**2) / (self.scale**2) 
         return error/nsim
 
-    def var(self, ndraw=10000):
+    def form_constraint(self):
         n, p = self.X.shape
         X, y, lam = self.X, self.y, self.lam
         active = (self.L.coef_ != 0)
@@ -61,26 +61,47 @@ class Error:
             lam * np.dot(X_notE.T, np.dot(X_Einv.T, signs))
         offset[p:] = lam + np.dot(X_notE.T, R_E).dot(y) + \
             lam * np.dot(X_notE.T, np.dot(X_Einv.T, signs))
-        con = affine.constraints(linear_part, offset,
+        self.con = affine.constraints(linear_part, offset,
                                  (self.scale*self.sigma)**2 * np.identity(n))
         #print (linear_part.dot(y_star - y) <= offset).all()
-        S = affine.sample_from_constraints(con, self.y_star-y, ndraw=ndraw)
-        pe = avg_pe(y - 1./(self.scale**2) * S, X.dot(self.L.coef_)) - \
-                (self.sigma**2) / (self.scale**2)
-        return pe
+
+
+    def var(self, ndraw=10000):
+        n, p = self.X.shape
+        self.form_constraint()
+        S = affine.sample_from_constraints(self.con, self.y_star-self.y, ndraw=ndraw)
+        error = 0
+        period = 20
+        for i in range(ndraw):
+            if i % period  == 0:
+                rvs = S[i,:].reshape(n)
+                ls = Lasso(self.lam/n, fit_intercept=False)
+                ls.fit(self.X, self.y+rvs)
+                pe = avg_pe(self.y - 1./(self.scale**2) * rvs, self.X.dot(ls.coef_)) - \
+                        (self.sigma**2) / (self.scale**2)
+                error += pe / (ndraw/period)
+        return error 
     
-    def unbiased(self, nsim=10):
+    def unbiased(self, nsim=100):
         n, p = self.X.shape
         beta0 = np.zeros(p)
         beta0[:self.s] = 7
-        y_new = self.X.dot(beta0) + np.random.standard_normal((nsim,n)) * self.sigma
-        return avg_pe(y_new, self.X.dot(self.L.coef_))
+        error = 0
+        for _ in range(nsim):
+            rvs = np.random.standard_normal(n)*self.sigma*self.scale
+            ls = Lasso(self.lam/n, fit_intercept=False)
+            ls.fit(self.X, self.y+rvs)
+            y_new = self.X.dot(beta0) + np.random.standard_normal((nsim,n)) * self.sigma
+            error += avg_pe(y_new, self.X.dot(ls.coef_)) / nsim
+        return error
 
 def avg_pe(y, y_hat):
     return ((y-y_hat)**2).mean()
 
-for _ in range(10):
+for _ in range(20):
     errors = Error()
+    avg = []
     print "true: ", errors.unbiased(), " cv: ", errors.cv(), " rand: ", errors.rand(), \
             " variables: ", errors.var()
-
+    avg.append([errors.unbiased(), errors.cv(), errors.rand(), errors.var()]) 
+print np.array(avg).mean(0)
